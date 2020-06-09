7db8903a9d1a6fa45a1aa1b89e0a5de20bf3c0af
albornet <alban.bornet@epfl.ch>
2020-06-10 00:23:01 +0200

    first commit

---
+++ dataset.py
@@ -0,0 +1,319 @@
+# Dataset funcions
+import numpy as np
+from numpy.random import RandomState as rng
+from skimage.draw import circle, ellipse, rectangle, polygon
+from skimage.transform import rotate
+
+# Ball class
+class Ball():
+
+	# Function to create the ball
+	def __init__(self, set_type, balls, batch_s, scale, n_chans, wn_h, wn_w, wall_d, grav):
+
+		# Select id, color, rebound fact, size, mass and gravity
+		self.colr  = rng().randint(    100,    255, (n_chans, batch_s))
+		self.fact  = rng().uniform(    0.8,    0.9, (1,       batch_s))
+		self.size  = rng().uniform(wn_w/14, wn_w/7, (1,       batch_s))
+		self.mass  = self.size**3
+
+		# Booleans initialization
+		self.tch_G = np.zeros((1, batch_s), dtype=bool)  # touches ground
+		self.tch_R = np.zeros((1, batch_s), dtype=bool)  # touches right wall
+		self.tch_L = np.zeros((1, batch_s), dtype=bool)  # touches left wall
+
+		# Select random initial position, velocity and acceleration
+		w        = wall_d
+		wn_d     = max(wn_w, wn_h)
+		s        = max(self.size)
+		x        = rng().uniform(s+w,       wn_d-s-w, (1, batch_s))
+		y        = rng().uniform(  0,       wn_d-s-w, (1, batch_s))
+		vx       = rng().uniform(-15*scale, 15*scale, (1, batch_s))
+		vy       = rng().uniform( -5*scale,  0*scale, (1, batch_s))
+		self.pos = np.vstack((x,   y))
+		self.vel = np.vstack((vx, vy))
+		self.acc = np.array([[0.00]*batch_s, [grav]*batch_s])
+
+		# Check for overlap with other balls
+		if len(balls) > 1:
+			bad = np.array([True]*batch_s)
+			while any(bad):
+				bad = np.array([False]*batch_s)
+				for ball in balls:
+					d = np.hypot(*(self.pos-ball.pos))
+					bad[(d < self.size + ball.size)[0]] = True
+
+				# Try new sizes and positions if the ball overlaps with another one
+				self.size[:, bad] = rng().uniform(wn_d/12, wn_d/6, (1, bad.sum()))
+				self.mass[:, bad] = self.size[:, bad]**3
+				s                 = max(self.size[:, bad])
+				x                 = rng().uniform(s+w, wn_w-s-w, (1, bad.sum()))
+				y                 = rng().uniform(  0, wn_h-s-w, (1, bad.sum()))
+				self.pos[:, bad]  = np.vstack((x, y))
+
+	# Take care of ball chocs
+	def compute_changes(self, balls, self_idx, wn_h, wn_w, wall_d):
+
+		# Check balls that move towards each other
+		for i in range(self_idx+1, len(balls)):
+			ball = balls[i]
+			if ball is not self:
+				r12 = self.pos - ball.pos
+				d   = np.hypot(*r12)
+				bad = (d < self.size + ball.size)[0]
+				if bad.sum() > 0:
+
+					# Balls hit each other
+					dT     = d[bad]
+					rT     = r12[:, bad]
+					v1, v2 = self.vel[ :, bad], ball.vel[ :, bad]
+					m1, m2 = self.mass[:, bad], ball.mass[:, bad]
+					vT     = ((v1-v2)*rT).sum(axis=0)
+					fT     = 1.0 - (1.0-self.fact[:, bad]) - (1.0-ball.fact[:, bad])
+					u1     = (2*m2/(m1+m2)*vT/(dT**2)*rT)
+					u2     = (2*m1/(m1+m2)*vT/(dT**2)*rT)
+					self.vel[:, bad]  = (self.vel[:, bad] - u1)*fT
+					ball.vel[:, bad]  = (ball.vel[:, bad] + u2)*fT
+
+		# Check balls that hit walls
+		self.tch_G = (self.pos[1] + 1 > -self.size-wall_d+wn_h)  # touches ground
+		self.tch_R = (self.pos[0] + 1 > -self.size-wall_d+wn_w)  # touches right wall
+		self.tch_L = (self.pos[0] - 1 <  self.size+wall_d     )  # touches left wall
+
+		# Check balls that touch each other
+		for i in range(0, len(balls)):
+			ball = balls[i]
+			if ball is not self:
+				r12  = self.pos - ball.pos
+				d    = np.hypot(*r12)
+				bad  = (d     < self.size + ball.size)[0]
+				badG = (d - 0 < self.size + ball.size)[0]
+				bad  = np.logical_and(bad, r12[1] > 0)
+				badG = np.logical_and(np.logical_and(badG, r12[1] > 0), self.tch_G[0])
+				if bad.sum() > 0:
+
+					# Correct ball positions
+					dT = d[bad]
+					rT = r12[:, bad]
+					cT = rT/dT*(self.size[:, bad] + ball.size[:, bad] - dT)
+					self.pos[:, bad ] += cT/2
+					ball.pos[:, bad ] -= cT/2
+
+					# Correct how gravity applies to balls on top (not working yet)
+					aT = self.acc[:, badG]
+					gT = r12[:, badG]/d[badG]        # unit vector
+					gT = np.vstack((-gT[1], gT[0]))  # perp unit vector
+					gT = (gT*aT).sum(axis=0)*gT      # perp vector scalar normed
+					ball.acc[:, badG] = gT
+
+		# Re-check and correct balls that hit walls
+		self.tch_G = (self.pos[1] > -self.size-wall_d+wn_h)  # touches ground
+		self.tch_R = (self.pos[0] > -self.size-wall_d+wn_w)  # touches right wall
+		self.tch_L = (self.pos[0] <  self.size+wall_d     )  # touches left wall
+		self.vel[1, self.tch_G[0]] *= -self.fact[self.tch_G]
+		self.vel[0, self.tch_R[0]] *= -self.fact[self.tch_R]
+		self.vel[0, self.tch_L[0]] *= -self.fact[self.tch_L]
+		self.pos[1, self.tch_G[0]]  = -self.size[self.tch_G] - wall_d + wn_h
+		self.pos[0, self.tch_R[0]]  = -self.size[self.tch_R] - wall_d + wn_w
+		self.pos[0, self.tch_L[0]]  =  self.size[self.tch_L] + wall_d
+
+	# Draw the ball (probably this can be done much more efficiently)
+	def draw(self, wn, batch_s):
+		for b in range(batch_s):  # (row, col) is (y, x)
+			rc, cc = circle(self.pos[1, b], self.pos[0, b], radius=self.size[0, b], shape=wn.shape[1:3])
+			for i, color in enumerate(self.colr[:, b]):
+				wn[b, rc, cc, i] = color
+			# p0 = self.pos[:, b].astype(int)
+			# p1 = (self.pos[:, b] + 5*self.acc[:, b]).astype(int)
+			# rc, cc = line(p0[1], p0[0], p1[1], p1[0])
+			# wn[b, rc, cc, :] = 0
+
+	# Update position and velocity, reset acceleration
+	def update_states(self, batch_s, friction, gravity):
+		self.vel += self.acc - self.vel*friction
+		self.pos += self.vel
+		self.acc  = np.array([[0.00]*batch_s, [gravity]*batch_s])
+
+
+# Neil class
+class Neil():
+
+	# Initialize object's properties
+	def __init__(self, set_type, objects, batch_s, scale, n_chans, wn_h, wn_w, wall_d, grav):
+
+		# Select id, color, sizes, orientations, etc.
+		if set_type == 'recons':
+			choices    = ['rectangle', 'ellipse', 'vernier']
+			self.ori   = rng().uniform(0, 2*np.pi, (1, batch_s))
+			self.sizx  = rng().uniform(wn_w/20, wn_w/5, (1, batch_s))
+			self.sizy  = rng().uniform(wn_w/20, wn_w/5, (1, batch_s))
+		if set_type == 'decode':
+			choices    = ['vernier']
+			self.vside = rng().randint(0, 2,             (1, batch_s)) if objects == [] else objects[0].vside
+			self.ori   = rng().uniform(0, 0.5*np.pi,     (1, batch_s))
+			self.sizx  = rng().uniform(wn_w/20, wn_w/10, (1, batch_s))
+			self.sizy  = rng().uniform(wn_w/10, wn_w/5,  (1, batch_s))
+		self.shape = rng().choice(choices,   (1,       batch_s))
+		self.colr  = rng().randint(100, 255, (n_chans, batch_s))
+		
+		# Select random initial position, velocity and acceleration
+		x        = rng().uniform( 2*wn_w/6, 4*wn_w/6, (1, batch_s))
+		y        = rng().uniform( 2*wn_h/6, 4*wn_h/6, (1, batch_s))
+		vx       = rng().uniform(-5*scale,  5*scale,  (1, batch_s))
+		vy       = rng().uniform(-5*scale,  5*scale,  (1, batch_s))
+		self.pos = np.vstack((x,   y))
+		self.vel = np.vstack((vx, vy))
+		self.acc = np.array([[0.00]*batch_s, [grav]*batch_s])
+
+		# Generate patches to draw the shapes efficiently
+		self.patches = []
+		for b in range(batch_s):
+			max_s = int(2*max(self.sizx[0, b], self.sizy[0, b]))
+			patch = np.zeros((max_s, max_s))
+			if self.shape[0, b] == 'ellipse':
+				center = (patch.shape[0]//2, patch.shape[1]//2)
+				radius = (self.sizy[0, b]/2, self.sizx[0, b]/2) 
+				rr, cc = ellipse(center[0], center[1], radius[0], radius[1], shape=patch.shape)
+				patch[rr, cc] = 255
+			elif self.shape[0, b] == 'rectangle':
+				start  = ((max_s - self.sizy[0, b])//2, (max_s - self.sizx[0, b])//2)
+				extent = (self.sizy[0, b], self.sizx[0, b])
+				rr, cc = rectangle(start=start, extent=extent, shape=patch.shape)
+				rr     = rr.astype(int)
+				cc     = cc.astype(int)
+				patch[rr, cc] = 255
+			# if self.shape == 'triangle':
+			# 	rr, cc = polygon()
+			if self.shape[0, b] == 'vernier':
+				vside    = rng().randint(0, 2) if set_type == 'recons' else self.vside[0, b]
+				v_off_w  = rng().randint(1, self.sizx[0, b]//5)
+				v_off_h  = rng().randint(0, self.sizy[0, b]//5)
+				v_siz_w  = (self.sizx[0, b] - v_off_w)//2
+				v_siz_h  = (self.sizy[0, b] - v_off_h)//2
+				start1   = ((max_s - self.sizy[0, b])//2, (max_s - self.sizx[0, b])//2)
+				start2   = (start1[0] + (self.sizy[0, b] + v_siz_h)//2,
+                            start1[1] + (v_siz_w)//2)
+				extent   = (v_siz_h, v_siz_w)
+				rr1, cc1 = rectangle(start=start1, extent=extent, shape=patch.shape)
+				rr2, cc2 = rectangle(start=start2, extent=extent, shape=patch.shape)
+				rr1      = rr1.astype(int)
+				cc1      = cc1.astype(int)
+				rr2      = rr2.astype(int)
+				cc2      = cc2.astype(int)
+				patch[rr1, cc1] = 255
+				patch[rr2, cc2] = 255
+				# if vside:  # 0 is R vernier and 1 is L vernier
+				# 	patch = np.fliplr(patch)
+
+				print(int(self.sizy[0, b]), max_s, start1[0], start2[0], v_off_h, v_siz_h)
+				print(int(self.sizx[0, b]), max_s, start1[1], start2[1], v_off_w, v_siz_w)
+
+			self.patches.append(rotate(patch, self.ori[0, b]).astype(int))
+			
+	# Compute what must be updated between the frames
+	def compute_changes(self, objects, self_idx, wn_h, wn_w, wall_d):
+		pass
+
+	# Draw the object (square patch)
+	def draw(self, wn, batch_s):
+		for b in range(batch_s):
+			patch  = self.patches[b]/255
+			start  = [self.pos[1, b] - patch.shape[0]//2, self.pos[0, b] - patch.shape[1]//2]
+			rr, cc = rectangle(start=start, extent=patch.shape, shape=wn.shape[1:3])
+			rr     = rr.astype(int)
+			cc     = cc.astype(int)
+			# rr, cc = rectangle(start=start, extent=patch.shape)
+			# rr     = np.mod(rr.astype(int), wn.shape[1])
+			# cc     = np.mod(cc.astype(int), wn.shape[2])
+			pat_rr = (rr - self.pos[1, b] - patch.shape[0]/2).astype(int)
+			pat_cc = (cc - self.pos[0, b] - patch.shape[1]/2).astype(int)
+			bckgrd = wn[b, rr, cc, :]
+			for i, color in enumerate(self.colr[:, b]):
+				col_patch = color*patch[pat_rr, pat_cc] - bckgrd[:,:,i]
+				wn[b, rr, cc, i] += col_patch.clip(0, 255).astype(np.uint8)
+
+	# Update objects position and velocity
+	def update_states(self, batch_s, friction, gravity):
+		self.vel += self.acc - self.vel*friction
+		# self.pos += self.vel
+
+
+# Class to generate batches of bouncing balls
+class BatchMaker():
+
+	# Initiates all values unchanged from batch to batch
+	def __init__(self, set_type, object_type, n_objects, batch_s, n_frames, im_dims):
+		object_dict = {
+		  'ball': {'generator': Ball, 'gravity': 1.00, 'wall_d': 4, 'friction': 0.01},
+		  'neil': {'generator': Neil, 'gravity': 0.00, 'wall_d': 0, 'friction': 0.00}}
+		object_descr   = object_dict[object_type]
+		self.Object    = object_descr['generator']
+		self.n_objects = n_objects
+		self.batch_s   = batch_s
+		self.n_frames  = n_frames
+		self.n_chans   = im_dims[-1]
+		self.scale     = max(im_dims[0], im_dims[1])/64
+		self.wn_h      = int(im_dims[0]*self.scale)
+		self.wn_w      = int(im_dims[1]*self.scale)
+		self.wall_d    = int(object_descr['wall_d']*self.scale)
+		self.gravity   = object_descr['gravity']*self.scale
+		self.friction  = object_descr['friction']
+		self.set_type  = set_type
+	
+	# Initialize batch, objects (size, position, velocities, etc.) and background
+	def init_batch(self):
+		self.batch   = []
+		self.objects = []
+		self.window  = 127*np.ones((self.batch_s, self.wn_h, self.wn_w, self.n_chans), dtype=np.uint8)
+		for _ in range(self.n_objects):
+			self.objects.append(self.Object(  self.set_type, self.objects, self.batch_s, self.scale,
+			                    self.n_chans, self.wn_h,     self.wn_w,    self.wall_d,  self.gravity))
+		self.bg_color = rng().randint(0, 80, (self.batch_s, self.n_chans))
+		for b in range(self.batch_s):
+			for c in range(self.n_chans):
+				self.window[b, :self.wn_h-self.wall_d, self.wall_d:self.wn_w-self.wall_d, c] = self.bg_color[b, c]
+
+	# Batch making function (generating batch_s dynamic sequences)
+	def generate_batch(self):
+		self.init_batch()
+		for t in range(self.n_frames):
+			frame = self.window*1
+			for i, obj in enumerate(self.objects):
+				obj.compute_changes(self.objects, i, self.wn_h, self.wn_w, self.wall_d)
+			for obj in self.objects:
+				obj.draw(frame, self.batch_s)
+			for obj in self.objects:
+				obj.update_states(self.batch_s, self.friction, self.gravity)
+			self.batch.append(frame)
+		return self.batch  # list of n_frames numpy arrays of dims [batch, h, w, channels]
+
+# Show example of reconstruction batch
+if __name__ == '__main__':
+
+  import pyglet   # conda install -c conda-forge pyglet
+  import imageio  # conda install -c conda-forge imageio
+  import os
+  object_type  = 'neil'
+  set_type     = 'decode'
+  n_objects    = 1
+  n_frames     = 20
+  scale        = 2
+  batch_s      = 1
+  n_channels   = 3
+  batch_maker  = BatchMaker(set_type, object_type, n_objects, batch_s, n_frames, (64*scale, 64*scale, n_channels))
+  batch_frames = batch_maker.generate_batch()
+  gif_name     = 'test_output.gif'
+
+  frames = []
+  for t in range(n_frames):
+  	frames.append(np.hstack([batch_frames[t][b] for b in range(batch_s)]))
+  imageio.mimsave(gif_name, frames, duration=0.1)
+  anim   = pyglet.resource.animation(gif_name)
+  sprite = pyglet.sprite.Sprite(anim)
+  window = pyglet.window.Window(width=sprite.width, height=sprite.height)
+  window.set_location(600, 300)
+  @window.event
+  def on_draw():
+  	window.clear()
+  	sprite.draw()
+  pyglet.app.run()
+  os.remove(gif_name)
---
+++ models.py
@@ -0,0 +1,246 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import imageio
+
+
+######################
+### Reconstructors ###
+######################
+
+# Simple "deconvolution" reconstructor
+my_recons = tf.keras.Sequential(
+    [tf.keras.layers.Dense(4*8*32),
+     tf.keras.layers.Reshape((4,8,32)),
+     tf.keras.layers.Conv2DTranspose(16, (5,5), strides=(1,1), padding='same', activation='relu'),
+     tf.keras.layers.Conv2DTranspose( 8, (5,5), strides=(2,2), padding='same', activation='relu'),
+     tf.keras.layers.Conv2DTranspose( 4, (5,5), strides=(2,2), padding='same', activation='relu'),
+     tf.keras.layers.Conv2DTranspose( 1, (5,5), strides=(2,2), padding='same', activation='relu')])
+
+
+################
+### Decoders ###
+################
+
+# Simple linear decoder
+my_decoder = tf.keras.Sequential(
+    [tf.keras.layers.Flatten(),
+     tf.keras.layers.BatchNormalization(),
+     tf.keras.layers.Dense(512, activation='relu'   ),
+     tf.keras.layers.Dense(  2, activation='softmax')])
+
+
+###################
+### Core models ###
+###################
+
+# Connected network of simple rate cells
+def simple_RNN(im_dims, n_frames, n_units):
+  return tf.keras.Sequential([
+         tf.keras.layers.InputLayer(input_shape=(n_frames,) + im_dims),
+         tf.keras.layers.Reshape((n_frames, tf.math.reduce_prod(im_dims))),
+         tf.keras.layers.SimpleRNN(units=n_units, return_sequences=True)])  # w/o return_seq: returns output only at last step (= last frame)
+
+
+# Connected network of LSTM units
+def simple_LSTM(im_dims, n_frames, n_units):
+  return tf.keras.Sequential([
+         tf.keras.layers.InputLayer(input_shape=(n_frames,) + im_dims),
+         tf.keras.layers.Reshape((n_frames, tf.math.reduce_prod(im_dims))),
+         tf.keras.layers.LSTM(units=n_units, return_sequences=True)])  # w/o return_seq: returns output only at last step (= last frame)
+
+
+# Connected network of GRU units
+def simple_GRU(im_dims, n_frames, n_units):
+  return tf.keras.Sequential([
+         tf.keras.layers.InputLayer(input_shape=(n_frames,) + im_dims),
+         tf.keras.layers.Reshape((n_frames, tf.math.reduce_prod(im_dims))),
+         tf.keras.layers.GRU(units=n_units, return_sequences=True)])  # w/o return_seq: returns output only at last step (= last frame)
+
+
+# Convolutional layer that integrates information through space and time
+def conv2D_LSTM(im_dims, n_frames, n_units):
+  return tf.keras.Sequential([
+         tf.keras.layers.InputLayer(input_shape=(n_frames,) + im_dims),
+         # tf.keras.layers.Reshape((n_frames, tf.math.reduce_prod(im_dims))),
+         tf.keras.layers.ConvLSTM2D(filters=16, kernel_size=(16,16), strides = (2,2), return_sequences=True, stateful=False, padding='same'),
+         tf.keras.layers.Reshape((n_frames, -1))])
+
+
+# Predictive coding network
+class PredNet(tf.keras.Model):
+  
+  def __init__(self,  R_channels, A_channels, t_extrapolate=float('inf')):
+  
+    super(PredNet, self).__init__()
+    self.r_channels    = R_channels + (0,)  # for convenience (last layer)
+    self.a_channels    = A_channels
+    self.n_layers      = len(R_channels)
+    self.t_extrapolate = t_extrapolate
+
+    for i in range(self.n_layers):  # number of input features: 2*self.a_channels[i] + self.r_channels[i+1]
+      cell = tf.keras.layers.ConvLSTM2D(filters=self.r_channels[i], kernel_size=(3,3), return_sequences=True, stateful=True, padding='same')
+      setattr(self, 'cell{}'.format(i), cell)
+
+    # for i in range(self.n_layers):
+    #   conv = tf.keras.layers.Conv2D(filters=self.a_channels[i], kernel_size=(3,3), padding='same', activation='relu')
+    #   if i == 0:
+    #     conv = tf.keras.Sequential([conv, SatLU()])
+    #   setattr(self, 'conv{}'.format(i), conv)
+    for i in range(self.n_layers):
+      conv = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(filters=self.a_channels[i], kernel_size=(3,3), padding='same', activation='relu'),
+        tf.keras.layers.BatchNormalization()])
+      if i == 0:
+        conv = tf.keras.Sequential([
+          tf.keras.Sequential([conv, SatLU()]),
+          tf.keras.layers.BatchNormalization()])
+      setattr(self, 'conv{}'.format(i), conv)
+
+    self.upsample = tf.keras.layers.UpSampling2D(size=(2,2))
+    self.maxpool  = tf.keras.layers.MaxPool2D(pool_size=(2,2), strides=(2,2))
+    for l in range(self.n_layers - 1):
+      update_A = tf.keras.Sequential([tf.keras.layers.Conv2D(self.a_channels[l+1], (3, 3), padding='same'), self.maxpool])
+      setattr(self, 'update_A{}'.format(l), update_A)
+
+  def set_t_extrapolate(self, t):
+    self.t_extrapolate = t
+
+  def call(self, x):
+
+    R_seq = [None]*self.n_layers
+    H_seq = [None]*self.n_layers
+    E_seq = [None]*self.n_layers
+    state = [None]*self.n_layers
+
+    h, w       = x.shape[-3], x.shape[-2]
+    batch_size = x.shape[0]
+    time_steps = x.shape[1]
+
+    for l in range(self.n_layers):
+      E_seq[l] = tf.zeros((batch_size,    h, w, 2*self.a_channels[l]))
+      R_seq[l] = tf.zeros((batch_size, 1, h, w, 1*self.r_channels[l]))
+      state[l] = tf.zeros((self.r_channels[l], self.r_channels[l]))
+      w = w//2
+      h = h//2
+    
+    frame_predictions = [[] for l in range(self.n_layers)]
+    for t in range(time_steps):
+
+      # Top-down pass to update LSTM states (R is LSTM neural state)
+      for l in reversed(range(self.n_layers)):
+        cell = getattr(self, 'cell{}'.format(l))
+        if t == 0:
+          try:
+            cell.reset_states()  # first time step does not want last state of previous sequence
+          except TypeError:
+            pass
+        E    = tf.expand_dims(E_seq[l], axis=1)
+        R    = R_seq[l]
+        if l == self.n_layers - 1:
+          R = cell(E)
+        else:
+          R = cell(tf.concat([E, tf.expand_dims(self.upsample(tf.squeeze(R_seq[l+1], axis=1)), axis=1)], axis=-1))
+        R_seq[l] = R
+
+      # Bottom-up pass to compute predictions A_hat and prediction errors (A_hat - A)
+      A = frame_predictions[0][-1] if t >= self.t_extrapolate else x[:,t]  # extrapolation makes forward zero
+      for l in range(self.n_layers):
+        conv     = getattr(self, 'conv{}'.format(l))
+        A_hat    = conv(tf.squeeze(R_seq[l], axis=1))
+        frame_predictions[l].append(A_hat)
+        pos      = tf.nn.relu(A_hat - A)
+        neg      = tf.nn.relu(A - A_hat)
+        E        = tf.concat([pos, neg], axis=-1)
+        E_seq[l] = E
+        if l < self.n_layers - 1:
+          update_A = getattr(self, 'update_A{}'.format(l))
+          A        = update_A(E)
+    
+    return [tf.stack(frame_predictions[l], axis=1) for l in range(self.n_layers)]
+
+# Helper function for prednet
+class SatLU(tf.keras.Model):
+    def __init__(self, lower=0, upper=1):
+        super(SatLU, self).__init__()
+        self.lower = lower
+        self.upper = upper
+    def call(self, input):
+        return tf.clip_by_value(input, self.lower, self.upper)
+
+
+####################
+### All together ###
+####################
+
+# Wrapper class to combine core model, reconstructor and decoder
+class Wrapper(tf.keras.Model):
+
+  def __init__(self, model, reconstructor, decoder, n_frames, model_name):
+    super(Wrapper, self).__init__()
+    self.model         = model
+    self.reconstructor = reconstructor
+    self.decoder       = decoder
+    self.n_frames      = n_frames
+    self.model_name    = model_name
+
+  def call(self, img):
+    x      = tf.tile(tf.expand_dims(img, axis=1), (1, self.n_frames, 1, 1, 1))
+    states = self.model(x)
+    return self.decoder(states[:,0])
+
+  def get_reconstructions(self, x):
+    if isinstance(self.model, PredNet):
+      return self.model(x)[0]
+    else:
+      x = tf.cast(x, tf.float32)
+      states = self.model(x)
+      recs   = []
+      for t in range(self.n_frames):
+        recs.append(self.reconstructor(states[:,t]))
+      return tf.stack(recs, axis=1)
+
+  def compute_rec_loss(self, x, recons):
+    weights = [1.0/(n+1) for n in range(self.n_frames-1)]  # first frame cannot be predicted
+    if isinstance(self.model, PredNet):                    # nth PredNet output predicts nth frame
+      if self.model.t_extrapolate < float('inf'):
+        weights = [w if n < self.model.t_extrapolate else 2.0*w for n, w in enumerate(weights)]
+      losses = [w*tf.reduce_sum((recons[:, n+1] - x[:, n+1])**2) for n, w in enumerate(weights)]
+    else:
+      losses = [w*tf.reduce_sum((recons[:, n] - x[:, n+1])**2) for n, w in enumerate(weights)]
+    frame_losses = [w*tf.reduce_sum((x[:, n] - x[:, n+1])**2) for n, w in enumerate(weights)]
+    return tf.reduce_sum(losses)/tf.reduce_sum(frame_losses)  # new idea!!
+
+  def train_step(self, x, b, e, opt):
+    with tf.GradientTape() as tape:
+      x        = tf.cast(x, tf.float32)
+      recs     = self.get_reconstructions(x)
+      rec_loss = self.compute_rec_loss(x, recs)
+    if isinstance(self.model, PredNet):  # Prednet generates reconstructions itself
+      vars_to_train = self.model.trainable_variables
+    else:
+      vars_to_train = self.model.trainable_variables + self.reconstructor.trainable_variables
+    grad = tape.gradient(rec_loss, vars_to_train)
+    opt.apply_gradients(zip(grad, vars_to_train))
+    if b == 0:
+      lr_str = "{:.2e}".format(opt._decayed_lr(tf.float32).numpy())
+      print('\nStarting epoch %03i, lr = %s, rec loss = %.3f' % (e, lr_str, rec_loss))
+      self.plot_output(x, recs)
+  
+  def plot_output(self, x, r):
+
+    # Plot frames
+    r = tf.clip_by_value(r, 0.0, 1.0)
+    f = plt.figure(figsize=(int(self.n_frames*(x.shape[3]+3)/32),int(2*(x.shape[2]+3)/32)))
+    for t in range(self.n_frames):
+      ax1 = f.add_subplot(2, self.n_frames+1, 0*(self.n_frames+1) + t + 1)
+      ax2 = f.add_subplot(2, self.n_frames+1, 1*(self.n_frames+1) + t + 1)
+      ax1.imshow(tf.squeeze(x[0,t]), cmap='Greys')  # squeeze and cmap only apply to n_channels = 1
+      ax2.imshow(tf.squeeze(r[0,t]), cmap='Greys')  # squeeze and cmap only apply to n_channels = 1
+    plt.savefig('./%s/latest_input_vs_prediction_epoch.png' % (self.model_name))
+    plt.close()
+
+    # Plot gifs
+    template  = tf.zeros((10 if i == 3 else x.shape[i] for i in range(len(x.shape))))  # black rectangle
+    xr        = tf.concat((x, template, r), axis=3)
+    xr_frames = [tf.cast(255*xr[0,t], tf.uint8).numpy() for t in range(self.n_frames)]
+    imageio.mimsave('./%s/latest_input_vs_prediction.gif' % (self.model_name), xr_frames, duration=0.1)
