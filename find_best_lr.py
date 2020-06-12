ceae02b19502e747022ec9cdb4d3285179db5bb5
Marie Anselmet <manselmet@gmail.com>
2020-06-12 15:28:10 +0200

    opt_lr

--- find_best_lr.py
+++ find_best_lr.py
@@ -5,6 +5,7 @@ from tensorflow.keras.callbacks import LearningRateScheduler
 import tensorflow.keras.backend as K
 import matplotlib.pyplot as plt
 import math
+import numpy as np
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'  # avoid printing GPU info messages
 os.environ['KMP_DUPLICATE_LIB_OK'] = '1' # MacOS pb
@@ -22,9 +23,11 @@ class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
     def __call__(self, step):
         return self.predefined_lr[int(step)-1]
 
-def find_best_lr(wrapp, obj_type, n_objs, im_dims, n_epochs, batch_size, n_batches, mode = 'decode', custom = True):
+def find_best_lr(wrapp, obj_type, n_objs, im_dims, n_epochs, batch_size, n_batches, mode = 'decode', verniers = False, custom = True):
 
-    batch_maker = BatchMaker(mode, obj_type, n_objs, batch_size, wrapp.n_frames, im_dims)
+    if verniers:
+        batch_maker = BatchMaker('decode', obj_type, n_objs, batch_size, wrapp.n_frames, im_dims)
+    else: batch_maker = BatchMaker(mode, obj_type, n_objs, batch_size, wrapp.n_frames, im_dims)
     
     # Learning devices
     init_lr = 1e-8
@@ -63,17 +66,17 @@ def find_best_lr(wrapp, obj_type, n_objs, im_dims, n_epochs, batch_size, n_batch
                 print('\nBatch %02i, lr = %s, dec loss = %.4f' % (b, updated_lr, loss))
 
 
+    print('Min loss for lr = %s, opt lr = %s' %(lrs[np.argmin(losses)], lrs[np.argmin(losses)]/10))
     # Plot the loss vs learning rate
     plot_output(lrs, losses, wrapp.model_name)
 
 def plot_output(lrs, losses, name):
-
     plt.figure()
     plt.ylabel("Loss")
     plt.xlabel("Learning rate (log scale)")
     plt.xscale("log")
     plt.plot(lrs, losses)
-    plt.show()
+    plt.grid()
     plt.savefig('./%s/loss_vs_lr.png' % (name))
     plt.close()
 
@@ -81,8 +84,8 @@ def plot_output(lrs, losses, name):
 
 if __name__ == '__main__':
 
-  obj_type    = 'neil'       # can be 'ball' or 'neil' for now
-  n_objs      = 2            # number of moving object in each sample
+  obj_type    = 'ball'       # can be 'ball' or 'neil' for now
+  n_objs      = 3            # number of moving object in each sample
   im_dims     = (64, 64, 3)  # image dimensions
   n_frames    = 20           # frames in the input sequences
   n_epochs    = 50          # epochs ran after latest checkpoint epoch
@@ -90,4 +93,4 @@ if __name__ == '__main__':
   n_batches   = 4           # batches per epoch
   model, name = PredNet((im_dims[-1], 32, 64, 128), (im_dims[-1], 32, 64, 128)), 'prednet2'
   wrapp       = Wrapper(model, my_recons, my_decoder, n_frames, name)
-  find_best_lr(wrapp, obj_type, n_objs, im_dims, n_epochs, batch_size, n_batches, 'decode')
+  find_best_lr(wrapp, obj_type, n_objs, im_dims, n_epochs, batch_size, n_batches, 'recons')
