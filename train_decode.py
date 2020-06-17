977fa6121fe9fa1aee2346eb6e0a0f67e33f248f
albornet <alban.bornet@epfl.ch>
2020-06-17 16:02:59 +0200

    use checkpoint to plot results

--- train_decode.py
+++ train_decode.py
@@ -15,58 +15,63 @@ def train_decode(wrapp, obj_type, n_objs, im_dims, n_epochs, batch_size, n_batch
     initial_learning_rate=init_lr, first_decay_steps=n_batches, t_mul=2.0, m_mul=0.95, alpha=0.3)
   optim = tf.keras.optimizers.Adam(sched)
 
-  # To store the accuracies
-  accs = tf.Variable(tf.zeros((100,)))
   # Checkpoint (save and load model weights and accuracies)
   model_dir    = '%s/%s/ckpt_model' % (os.getcwd(), wrapp.model_name)
   decoder_dir  = '%s/%s/ckpt_decod' % (os.getcwd(), wrapp.model_name)
   ckpt_model   = tf.train.Checkpoint(net=wrapp.model)
-  ckpt_decoder = tf.train.Checkpoint(optimizer=optim, net=wrapp.decoder, acc=accs)
+  ckpt_decoder = tf.train.Checkpoint(optim=optim, net=wrapp.decoder, losses=tf.Variable(tf.zeros((1000,))), accs=tf.Variable(tf.zeros((1000,))), )
   mngr_model   = tf.train.CheckpointManager(ckpt_model,   directory=model_dir,   max_to_keep=1)
   mngr_decoder = tf.train.CheckpointManager(ckpt_decoder, directory=decoder_dir, max_to_keep=1)
-  ckpt_model  .restore(mngr_model  .latest_checkpoint)
+
+  # Try to load latest checkpoints (if required)
+  ckpt_model.restore(mngr_model.latest_checkpoint).expect_partial()
   if mngr_model.latest_checkpoint:
-    print('\nReconstruction model loaded from %s' % (mngr_decoder.latest_checkpoint))
+    print('\nReconstruction model loaded from %s\n' % (mngr_model.latest_checkpoint))
   else:
-    print('\nWarning: your reconstruction model is not trained yet. Loading from scratch.')
+    print('\nWarning: your reconstruction model is not trained yet. Loading from scratch\n')
   if not from_scratch:
     ckpt_decoder.restore(mngr_decoder.latest_checkpoint)
     if mngr_decoder.latest_checkpoint:
-      print('\nDecoder restored from %s' % (mngr_decoder.latest_checkpoint))
+      print('Decoder restored from %s\n' % (mngr_decoder.latest_checkpoint))
   if from_scratch or not mngr_decoder.latest_checkpoint:
-    print('\nDecoder initialized from scratch')
+    print('Decoder initialized from scratch\n')
   if not os.path.exists('./%s' % (wrapp.model_name,)):
     os.mkdir('./%s' % (wrapp.model_name,))
 
   # Training loop for the decoder part
-  batch_maker = BatchMaker('decode', 'neil', n_objs, batch_size, wrapp.n_frames, im_dims)
-  accs_ = np.zeros(10)
-  saved_epochs = optim.iterations.numpy()
-  for e in range(n_epochs):
-    # Mean batch accuracy
-    mean_acc = 0.0
-    opt_e = optim.iterations/n_batches
-    if opt_e % 10 == 0 and e > 0:
-      saved_epochs += 1
-      acc_tens = tf.concat([tf.zeros((saved_epochs-1,)), tf.constant([np.mean(accs_)], shape=(1,), dtype='float32'), tf.zeros((100-saved_epochs,))], axis=0)
-      accs.assign_add(tf.Variable(acc_tens))
-      mngr_decoder.save()
-      print('\nCheckpoint saved at %s' % (mngr_decoder.latest_checkpoint,))
-      accs_ = np.zeros(10)
+  batch_maker  = BatchMaker('decode', 'neil', n_objs, batch_size, wrapp.n_frames, im_dims)
+  for _ in range(n_epochs):
+    e = ckpt_decoder.optim.iterations//n_batches
+
+    # Train the decoder for one epoch
+    mean_loss = 0.0
+    mean_acc  = 0.0
     for b in range(n_batches):  # batch shape: (batch_s, n_frames) + im_dims
       batch, labels = batch_maker.generate_batch()
-      acc, dec_loss = wrapp.train_step(tf.stack(batch, axis=1)/255, b, optim, labels, -1)
-      mean_acc += acc
-      if b == 0:    
-        lr_str = "{:.2e}".format(optim._decayed_lr(tf.float32).numpy())
-        print('\nStarting epoch %03i, lr = %s, accuracy = %.3f, dec loss = %.3f' % (opt_e, lr_str, acc, dec_loss))
-      print('\r Running batch %02i/%2i' % (b+1, n_batches), end='')
-    accs_[int(opt_e)] = mean_acc
+      acc, loss     = wrapp.train_step(tf.stack(batch, axis=1)/255, b, ckpt_decoder.optim, labels, -1)
+      mean_loss    += loss
+      mean_acc     += acc
+      print('\rRunning batch %02i/%02i' % (b+1, n_batches), end='')
 
-  # Plot accuracies vs epochs
-  if saved_epochs > 1:
-    wrapp.plot_results(np.arange(0, saved_epochs*10, 10), accs.numpy()[:saved_epochs], 'epoch', 'accuracy', 'decode')
-  
+    # Record loss for this epoch
+    mean_loss = mean_loss/n_batches
+    mean_acc  = mean_acc /n_batches
+    lr_str = "{:.2e}".format(ckpt_decoder.optim._decayed_lr(tf.float32).numpy())
+    print('\n  Finishing epoch %03i, lr = %s, accuracy = %.3f, loss = %.3f' % (e, lr_str, mean_acc, mean_loss))
+    loss_tens = tf.concat([tf.zeros((e,)), mean_loss*tf.ones((1,)), tf.zeros((1000-e-1,))], axis=0)
+    acc_tens  = tf.concat([tf.zeros((e,)), mean_acc *tf.ones((1,)), tf.zeros((1000-e-1,))], axis=0)
+    ckpt_decoder.losses.assign_add(tf.Variable(loss_tens))
+    ckpt_decoder.accs  .assign_add(tf.Variable( acc_tens))
+    
+    # Save checkpoint if necessary
+    if e % 10 == 9:
+      mngr_decoder.save()
+      print('\nDecoder checkpoint saved at %s' % (mngr_decoder.latest_checkpoint,))
+
+  # Plot loss and accuracy curves
+  wrapp.plot_results(range(e+1), ckpt_decoder.losses.numpy()[:e+1], 'epoch', 'loss', 'decode')
+  wrapp.plot_results(range(e+1), ckpt_decoder.  accs.numpy()[:e+1], 'epoch', 'acc' , 'decode')
+
 
 if __name__ == '__main__':
 
@@ -74,10 +79,10 @@ if __name__ == '__main__':
   n_objs      = 2            # number of moving object in each sample
   im_dims     = (64, 64, 1)  # image dimensions
   n_frames    = 10           # frames in the input sequences
-  n_epochs    = 100          # epochs ran after latest checkpoint epoch
+  n_epochs    = 100          # epochs ran IN ADDITION TO latest checkpoint epoch
   batch_size  = 16           # sample sequences sent in parallel
   n_batches   = 64           # batches per epoch
-  init_lr     = 1e-5         # first parameter to tune if does not work
+  init_lr     = 1e-3         # first parameter to tune if does not work
   model, name = PredNet((im_dims[-1], 32, 64, 128), (im_dims[-1], 32, 64, 128)), 'prednet2'
   decoder     = conv_decoder()
   wrapp       = Wrapper(model, my_recons, decoder, n_frames, name)
--- train_recons.py
+++ train_recons.py
@@ -14,64 +14,65 @@ def train_recons(wrapp, obj_type, n_objs, im_dims, n_epochs, batch_size, n_batch
   sched = tf.keras.experimental.CosineDecayRestarts(
     initial_learning_rate=init_lr, first_decay_steps=n_batches, t_mul=2.0, m_mul=0.95, alpha=0.3)
   optim = tf.keras.optimizers.Adam(sched)
-
-  # To store the losses 
-  losses = tf.Variable(tf.zeros((100,)))
-  # Checkpoint (save and load model weights and losses)
+  
+  # Initialize checkpoint
   model_dir  = '%s/%s/ckpt_model' % (os.getcwd(), wrapp.model_name)
-  ckpt_model = tf.train.Checkpoint(optimizer=optim, net=wrapp.model, loss=losses)
+  ckpt_model   = tf.train.Checkpoint(optim=optim, net=wrapp.model, losses=tf.Variable(tf.zeros((1000,))),)
   mngr_model = tf.train.CheckpointManager(ckpt_model, directory=model_dir, max_to_keep=1)
+  
+  # Try to load latest checkpoint (if required)
   if not from_scratch:
     ckpt_model.restore(mngr_model.latest_checkpoint)
     if mngr_model.latest_checkpoint:
-      print('\nModel %s restored from %s' % (wrapp.model_name, mngr_model.latest_checkpoint))
+      print('\nModel %s restored from %s\n' % (wrapp.model_name, mngr_model.latest_checkpoint))
     else:
-      print('\nModel %s initialized from scratch' % (wrapp.model_name))
+      print('\nModel %s initialized from scratch\n' % (wrapp.model_name))
       if not os.path.exists('./%s' % (wrapp.model_name,)):
         os.mkdir('./%s' % (wrapp.model_name,))
   else:
-    print('\nModel %s initialized from scratch' % (wrapp.model_name))
+    print('\nModel %s initialized from scratch\n' % (wrapp.model_name))
     if not os.path.exists('./%s' % (wrapp.model_name,)):
       os.mkdir('./%s' % (wrapp.model_name,))
+  
   # Training loop for the reconstruction part
   batch_maker = BatchMaker('recons', obj_type, n_objs, batch_size, wrapp.n_frames, im_dims)
-  losses_ = np.zeros(10)
-  saved_epochs = (optim.iterations/n_batches)%10
-  for e in range(n_epochs):
-    # Mean batch loss
-    mean_loss = 0.0
-    if e % 10 == 0 and e > 0:
-      saved_epochs += 1
-      loss_tens = tf.concat([tf.zeros((saved_epochs-1,)), tf.constant([np.mean(losses_)], shape=(1,), dtype='float32'), tf.zeros((100-saved_epochs,))], axis=0)
-      losses.assign_add(tf.Variable(loss_tens))
-      mngr_model.save()
-      print('\nCheckpoint saved at %s' % (mngr_model.latest_checkpoint,))
-      losses_ = np.zeros(10)
-    for b in range(n_batches):  # batch shape: (batch_s, n_frames) + im_dims
-      batch = tf.stack(batch_maker.generate_batch(), axis=1)/255
-      rec_loss = wrapp.train_step(batch, b, optim)
-      mean_loss += rec_loss
-      if b == 0:
-        lr_str = "{:.2e}".format(optim._decayed_lr(tf.float32).numpy())
-        print('\nStarting epoch %03i, lr = %s, rec loss = %.3f' % (e, lr_str, rec_loss))
-      print('\r  Running batch %02i/%2i' % (b+1, n_batches), end='')
-    losses_[e%10] = mean_loss
+  for _ in range(n_epochs):
+    e = ckpt_model.optim.iterations//n_batches
 
-  # Plot the loss vs epochs
-  if saved_epochs > 1:
-    wrapp.plot_results(np.arange(0, saved_epochs*10, 10), losses.numpy()[:saved_epochs], 'saved epoch', 'loss', 'recons')
+    # Train the model for one epoch
+    mean_loss = 0.0
+    for b in range(n_batches):  # batch shape: (batch_s, n_frames) + im_dims
+      batch      = tf.stack(batch_maker.generate_batch(), axis=1)/255
+      rec_loss   = wrapp.train_step(batch, b, ckpt_model.optim)
+      mean_loss += rec_loss
+      print('\rRunning batch %02i/%02i' % (b+1, n_batches), end='')
+
+    # Record loss for this epoch
+    mean_loss = mean_loss/n_batches
+    lr_str    = "{:.2e}".format(ckpt_model.optim._decayed_lr(tf.float32).numpy())
+    print('\n  Finishing epoch %03i, lr = %s, loss = %.3f' % (e, lr_str, mean_loss))
+    loss_tens = tf.concat([tf.zeros((e,)), mean_loss*tf.ones((1,)), tf.zeros((1000-e-1,))], axis=0)
+    ckpt_model.losses.assign_add(tf.Variable(loss_tens))
+
+    # Save checkpoint if necessary
+    if e % 10 == 9:
+      mngr_model.save()
+      print('\nModel checkpoint saved at %s' % (mngr_model.latest_checkpoint,))
+
+  # Plot the loss curve
+  wrapp.plot_results(range(e+1), ckpt_model.losses.numpy()[:e+1], 'epoch', 'loss', 'recons')
 
 
 if __name__ == '__main__':
 
-  obj_type    = 'neil'       # can be 'ball' or 'neil' for now
-  n_objs      = 2            # number of moving object in each sample
-  im_dims     = (64, 64, 1)  # image dimensions
-  n_frames    = 5            # frames in the input sequences
-  n_epochs    = 100          # epochs ran after latest checkpoint epoch
-  batch_size  = 16           # sample sequences sent in parallel
-  n_batches   = 64           # batches per epoch
-  init_lr     = 2e-4         # first parameter to tune if does not work
-  model, name = PredNet((im_dims[-1], 32, 64, 128), (im_dims[-1], 32, 64, 128)), 'prednet2'
-  wrapp       = Wrapper(model, my_recons, None, n_frames, name)
+  obj_type     = 'neil'       # can be 'ball' or 'neil' for now
+  n_objs       = 2            # number of moving object in each sample
+  im_dims      = (64, 64, 1)  # image dimensions
+  n_frames     = 10           # frames in the input sequences
+  n_epochs     = 100          # epochs ran IN ADDITION TO latest checkpoint epoch
+  batch_size   = 16           # sample sequences sent in parallel
+  n_batches    = 64           # batches per epoch
+  init_lr      = 2e-4         # first parameter to tune if does not work
+  model, name  = PredNet((im_dims[-1], 32, 64, 128), (im_dims[-1], 32, 64, 128)), 'prednet2'
+  wrapp        = Wrapper(model, my_recons, None, n_frames, name)
   train_recons(wrapp, obj_type, n_objs, im_dims, n_epochs, batch_size, n_batches, init_lr, from_scratch=False)
