1982637da2a89993ed952b7faa164ec4c5380007
Marie Anselmet <manselmet@gmail.com>
2020-06-22 15:16:39 +0200

    entropy

--- models.py
+++ models.py
@@ -277,7 +277,7 @@ class Wrapper(tf.keras.Model):
     if b == 0:
       self.plot_recons(x)
       #self.plot_state_all_layers(x)
-      self.plot_state_layer(x)
+      #self.plot_state_layer(x)
       self.plot_entropies(x)
       self.plot_distrubution_activities_lat_vars(x)
     if labels is not None:
@@ -368,7 +368,7 @@ class Wrapper(tf.keras.Model):
 
   def compute_entropy(self, x, layer=-1):
     entropies = []
-    lat_vars  = self.model(x)[layer][0] # sample 0
+    #lat_vars  = self.model(x)[layer][0] # sample 0
 
     for t in range(self.n_frames):
       """
@@ -378,24 +378,29 @@ class Wrapper(tf.keras.Model):
       normalized_lat_vars = (flat_lat_vars - min_) / (max_ - min_)
       """
       
+      ## BONNE VERSION:
+      """
       min_                         = tf.math.reduce_min(tf.keras.backend.flatten(lat_vars[:3,:,:,:]))
       max_                         = tf.math.reduce_max(tf.keras.backend.flatten(lat_vars[:3,:,:,:]))
       norm_lat_vars                = ((tf.keras.backend.flatten(lat_vars[t,:,:,:]) - min_) / (max_ - min_)).numpy() ## pas ouf
       prob_lat_vars                = norm_lat_vars/np.sum(norm_lat_vars)
-      
 
-      
-      
-      entropies.append(entropy(prob_lat_vars))
+    entropies.append(entropy(prob_lat_vars))
+      """
 
-      #print(entropies)
+      flat_lat_vars  = tf.keras.backend.flatten(self.model(x)[layer][0, t]).numpy()
+      hist, _        = np.histogram(flat_lat_vars, bins=100, density=True)
+      hist          /= np.sum(hist)
+      entropy        = -1.0*np.sum(hist * np.log(hist + np.finfo(float).eps))
+
+      entropies.append(entropy)
     return entropies
 
   def plot_entropies(self, x, layer=-1):
     plt.figure()
     plt.ylabel('Entropy')
     plt.xlabel('Frame')
-    plt.plot(range(self.n_frames), self.compute_entropy(x, layer)) #, 'go', linewidth=2, markersize=6)
+    plt.plot(range(1, self.n_frames), self.compute_entropy(x, layer)[1:]) #, 'go', linewidth=2, markersize=6)
     plt.grid()
     plt.savefig('./%s/entropy_vs_frames.png' % (self.model_name))
     #plt.show()
@@ -406,18 +411,9 @@ class Wrapper(tf.keras.Model):
 
     for t in range(self.n_frames):
       flat_lat_vars  = tf.keras.backend.flatten(self.model(x)[layer][0, t]).numpy()
-      axes[t].hist(flat_lat_vars, bins=100, range=(np.amin(flat_lat_vars), np.amax(flat_lat_vars)))#range=(-0.075, 0.075)) #(tf.reduce_min(flat_lat_vars), tf.reduce_max(flat_lat_vars)))
+      axes[t].hist(flat_lat_vars, bins=100, range=(-0.075, 0.075))  #range=(np.amin(flat_lat_vars), np.amax(flat_lat_vars)))#range=(-0.075, 0.075)) 
       axes[t].set(xlabel = 'Values of neuron activities at frame ' + str(t+1), ylabel = 'Occurence')
       axes[t].grid()
+
     
     fig.savefig('./%s/distribution_of_neuron_activities.png' % (self.model_name))
-
-
-
-
-  
-
-
-
-
-
