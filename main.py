ceadd2c7c2f4aa8f6f45293e6053bc5f62279388
albornet <alban.bornet@epfl.ch>
2020-07-07 09:44:02 +0200

    colab compatibility

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import tensorflow as tf
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'   # avoid printing GPU info messages
 os.environ['KMP_DUPLICATE_LIB_OK'] = '1'   # MacOS pb
-# from google.colab import drive           # UNCOMMENT THIS IN COLAB
-# drive.mount('/content/drive')            # UNCOMMENT THIS IN COLAB
-# %cd /content/drive/My\ Drive/sqm_models  # UNCOMMENT THIS IN COLAB
+''' # COMMENT THIS LINE IF ON COLAB
+from google.colab import drive
+drive.mount('/content/drive')
+%cd /content/drive/My\ Drive/sqm_models
+''' # COMMENT THIS LINE IF ON COLAB
 from dataset      import BatchMaker
 from models       import *
 from find_best_lr import find_best_lr
@@ -22,16 +24,16 @@ n_frames_recons  = [8, 13, 20]       # frames in the input sequences for reconst
 n_frames_decode  = 13                # frames in the input sequences for decoding
 n_frames_sqm     = 13                # frames in the input sequences for the sqm paradigm
 n_epochs_recons  = 20                # epochs ran after latest checkpoint epoch (for every frame number)
-n_epochs_decode  = 50                # epochs ran after latest checkpoint epoch
+n_epochs_decode  = 100               # epochs ran after latest checkpoint epoch
 n_objs_recons    = 10                # number of moving objects in recons batches
 n_objs_decode    = 2                 # number of moving objects in decode batches
 n_objs_sqm       = 2                 # number of moving objects in sqm paradigm
-noise_lvl_recons = 0.8               # amount of noise added to reconstruction set samples
-noise_lvl_decode = 0.8               # amount of noise added to decoding set samples
+noise_lvl_recons = 0.9               # amount of noise added to reconstruction set samples
+noise_lvl_decode = 0.1               # amount of noise added to decoding set samples
 noise_lvl_sqm    = 0.1               # amount of noise added to sqm samples
-init_lr_recons   = 2e-4              # first parameter to tune if does not work
-init_lr_decode   = 1e-4              # first parameter to tune if does not work
-do_train_recons  = True
+init_lr_recons   = 5e-4              # first parameter to tune if does not work
+init_lr_decode   = 1e-5              # first parameter to tune if does not work
+do_train_recons  = False
 do_train_decode  = True
 do_test_sqm      = True
 do_find_best_lr  = False
@@ -63,7 +65,7 @@ if do_train_decode:
 if do_test_sqm:
   wrapp.n_frames = n_frames_sqm
   wrapp.set_noise(noise_lvl_sqm)
-  test_sqm(wrapp, n_objs_sqm, im_dims, batch_size, n_batches, 'V', sqm_n_trials)
+  test_sqm(wrapp, n_objs_sqm, im_dims, batch_size, n_batches, 'V')
   for sec_frame in range(3, n_frames_sqm):
-    test_sqm(wrapp, n_objs_sqm, im_dims, batch_size, n_batches, 'V-PV' + str(sec_frame), sqm_n_trials)
-    test_sqm(wrapp, n_objs_sqm, im_dims, batch_size, n_batches, 'V-AV' + str(sec_frame), sqm_n_trials)
+    test_sqm(wrapp, n_objs_sqm, im_dims, batch_size, n_batches, 'V-PV' + str(sec_frame))
+    test_sqm(wrapp, n_objs_sqm, im_dims, batch_size, n_batches, 'V-AV' + str(sec_frame))
